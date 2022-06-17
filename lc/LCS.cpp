9de50826f9b3dd77def241161f0b8e09f7e191c9
John Wee <0728john@gmail.com>
2022-06-17 15:18:28 +0800

    LCS

---
+++ lc/LCS.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+// Not exactly the leetcode question, because this LCS prints out the string itself rather than length
+// Length is found before the string.
+class Solution {
+public:
+    int longestCommonSubsequence(string s1, string s2) {
+        vector<vector<int>> dp (s1.size()+1,vector<int> (s2.size()+1,0));
+        string ans = "";
+        for(int i=1;i<s1.size()+1;++i){
+            for(int j=1;j<s2.size()+1;++j){
+                if(s1[i-1]==s2[i-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                    continue;
+                }
+                dp[i][j] = max(dp[i-1][j],dp[i][j-1]); //If the newest char doesnt match we just take the longest subsequence of the preceding one.
+            }
+        }
+        return dp[s1.size()][s2.size()];
+    }
+};
+
+int main(){
+    Solution s;
+    string s1 = "abcde";
+    string s2 = "ace";
+    cout << s.longestCommonSubsequence(s1,s2);
+}
--- lc/triangle.cpp
+++ lc/triangle.cpp
@@ -3,6 +3,9 @@
 #include <algorithm>
 using namespace std;
 
+//Not bad - managed to work out a bottom up solution on the first try
+//Question could be more challenging if we had to return an array containing the min path
+
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
@@ -11,7 +14,7 @@ public:
         for (int i=1;i<triangle.size();++i){
             for (int j=0;j<triangle[i].size();++j){
                 x=0;
-                if(j<triangle[i-1].size()-1 && j>0){
+                if(j<triangle[i-1].size() && j>0){
                     x = triangle[i][j] + min(sum[i-1][j],sum[i-1][j-1]);
                 }
                 else if(j==0){
