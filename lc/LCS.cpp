af8f5806f56c41ff1a2cc6cd380af576d4ab4772
John Wee <0728john@gmail.com>
2022-06-17 15:47:09 +0800

    LCS

--- lc/LCS.cpp
+++ lc/LCS.cpp
@@ -11,12 +11,17 @@ public:
         string ans = "";
         for(int i=1;i<s1.size()+1;++i){
             for(int j=1;j<s2.size()+1;++j){
-                if(s1[i-1]==s2[i-1]){
+                if(s1[i-1]==s2[j-1]){
                     dp[i][j] = 1+dp[i-1][j-1];
-                    continue;
                 }
-                dp[i][j] = max(dp[i-1][j],dp[i][j-1]); //If the newest char doesnt match we just take the longest subsequence of the preceding one.
+                else dp[i][j] = max(dp[i-1][j],dp[i][j-1]); //If the newest char doesnt match we just take the longest subsequence of the preceding one.
             }
+        }
+        for(int i=0;i<s1.size()+1;++i){
+            for(int j=0;j<s2.size()+1;++j){
+                cout << dp[i][j] << " ";
+            }
+            cout << "\n";
         }
         return dp[s1.size()][s2.size()];
     }
